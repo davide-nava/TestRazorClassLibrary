f30c8d893887961f06015846057f40b0b639775b
Davide <davide.nava@nexus-schweiz.ch>
2021-10-24 10:43:54 +0200

    Add project files.

---
+++ TestRazorClassLibrary/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestRazorClassLibrary.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
